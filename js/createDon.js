381a22cb7bc53e528fe2c2db58a49ddef1f8081b
Toughee <MJ1menez@protonmail.com>
2020-06-11 00:43:24 -0500

    Don/004/Repo_cleaning - Cleaned js code for app - Updated README information

---
+++ js/createDon.js
@@ -0,0 +1,14 @@
+function new_Don() {
+
+    var taskd = document.getElementById('task_name').value;
+    var dated = document.getElementById('due_name').value;
+    var listedDon = '<b>' + taskd + '</b> ' + ", Due date: " + dated;
+
+    document.getElementById('result').innerHTML = listedDon;
+
+    if (taskd == "") {
+        alert("Please input your task");
+    }
+    return false;
+
+document.getElementById('create_Don').addEventListener('click', new_Don);
