6af7307cf06f66c563c4ffd0cbbf3b9ced15d92f
Toughee <MJ1menez@protonmail.com>
2020-07-15 14:36:41 -0500

    Don/009/UI-update - Did a moderate sized redesign of app, ranging from font choice to structure - Streamlined code into one file - Updated logo for app

--- js/createDon.js
+++ js/createDon.js
@@ -1,12 +1,26 @@
 function new_Don() {
 
     var taskdata = document.getElementById('task_name').value;
-    var datedata = document.getElementById('due_name').value;
+    //var datedata = document.getElementById('due_name').value;
+
 
 
     var createDon = document.createElement('p');
-    
-    var listedDon = document.createTextNode(' Task: ' + taskdata + ', Due date:' + datedata);
+
+    //var listedDon = document.createTextNode(' Task: ' + taskdata + ', Due date:' + datedata);
+    var listedDon = document.createTextNode('[X] ' + taskdata);
+
+
     createDon.appendChild(listedDon);
+
     document.getElementById("donSection").appendChild(createDon);
 }
+
+
+function deleteDon() {
+
+}
+
+var dn = document.createElement("button");
+
+dn.appendChild(document.createTextNode("X"));
