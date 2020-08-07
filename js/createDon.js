31c339f0f02b52b954a889eb8c602861db1992e9
Toughee <MJ1menez@protonmail.com>
2020-08-06 22:09:29 -0500

    Don/012/Documentation_update - Updated Documentation for project - Temporarily moved React files away from project - Removed unused webpage files from project

--- js/createDon.js
+++ js/createDon.js
@@ -1,26 +1,53 @@
 function new_Don() {
 
-    var taskdata = document.getElementById('task_name').value;
-    //var datedata = document.getElementById('due_name').value;
-
-
-
     var createDon = document.createElement('p');
-
-    //var listedDon = document.createTextNode(' Task: ' + taskdata + ', Due date:' + datedata);
-    var listedDon = document.createTextNode('[X] ' + taskdata);
-
+    var taskdata = document.getElementById('task_name').value;
+    // var deleteDon = document.createElement('button');
+    var listedDon = document.createTextNode('x   ' + taskdata);
 
     createDon.appendChild(listedDon);
 
     document.getElementById("donSection").appendChild(createDon);
+
+    function crossOut() {
+        li.classList.toggle("done");
+    }
+
+    p.addEventListener("click", crossOut);
+    //END STRIKETHROUGH
+
+
+    // START ADD DELETE BUTTON
+    var dBtn = document.createElement("button");
+    dBtn.appendChild(document.createTextNode("X"));
+    p.appendChild(dBtn);
+    dBtn.addEventListener("click", deleteListItem);
+    // END ADD DELETE BUTTON
+
+
+    //ADD CLASS DELETE (DISPLAY: NONE)
+    function deleteListItem() {
+        p.classList.add("delete")
+    }
 }
 
+// remover.textContent = 'x ';
+// remover.classList.add('button', 'button-text');
+// listedDon.appendChild(remover);
+// remover.addEventListener('click', () =>)
 
-function deleteDon() {
+// function xButton() {
+//     p.classList.toggle("done");
+// }
 
-}
+// p.addEventListener("click", xButton);
 
-var dn = document.createElement("button");
+//     var dn = document.createElement("button");
+//     dn.appendChild(document.createTextNode("x"));
+//     p.appendChild(dn);
+//     dn.addEventListener("click", deleteDon);
 
-dn.appendChild(document.createTextNode("X"));
+//     function deleteDon() {
+//         p.classList.add("delete")
+//     }
+// }
